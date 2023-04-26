7bba70761d12caa9d77a5f27fb141f7bccd4a314
Billy Byiringiro <billy@Billys-MacBook-Air.local>
2023-04-26 21:41:02 +0100

    updating the project page

--- src/portfolio.js
+++ src/portfolio.js
@@ -432,7 +432,7 @@ const experience = {
 
 // Projects Page
 const projectsHeader = {
-  title: "Projects",
+  title: "Personal Projects",
   description:'',
     // "My projects makes use of vast variety of latest technology tools. My best experience is to create Data Science projects and deploy them to web applications using cloud infrastructure.",
   avatar_image_path: "projects_image.svg",
